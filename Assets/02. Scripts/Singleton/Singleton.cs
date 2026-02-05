e8a38f762e5cb0e9fdb1f3d167e34d7bc600ca72
namgyeongtae <gotsuebel@gmail.com>
2026-02-05 15:07:11 +0900

    Add Singleton

---
+++ Assets/02. Scripts/Singleton/Singleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindFirstObjectByType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected abstract void OnSceneLoaded(Scene scene, LoadSceneMode mode);
+}
